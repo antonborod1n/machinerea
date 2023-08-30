05166aa01da46353b7a27ed66b88911e2e4500a1
antonborod1n <antonborodin86@gmail.com>
2023-08-30 20:27:28 +0300

    added offer page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+import './scss/main-offer.scss';
 import './scss/main.scss';
-import './scss/main-product.scss';
+
+const tabsBtn = document.querySelectorAll('.tabs__btn');
+
+tabsBtn.forEach((item) => {
+  item.addEventListener('click', () => {
+    tabsBtn.forEach((element) => {
+      if (element.classList.contains('is-active')) {
+        element.classList.remove('is-active');
+      }
+    });
+    item.classList.add('is-active');
+  });
+});
