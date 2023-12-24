10d5dd94b0063be06325156034f744342a1640cf
antonborod1n <antonborodin86@gmail.com>
2023-12-24 21:29:37 +0300

    fix header height, bredcrumbs, color rewievs

--- dist/js/sort.js
+++
@@ -1,8 +0,0 @@
-//start - sort
-const sortDropdownMenu = document.querySelector('.sort__dropdown');
-const offerSortBtn = document.querySelector('.offer__sort-btn-new');
-
-offerSortBtn.addEventListener('click', function () {
-  sortDropdownMenu.classList.toggle('is-active');
-});
-//end - sort
